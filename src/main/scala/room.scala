8392b25b7a8a495967b8318ef61157b83ad5a236
dennis84 <d.dietrich84@gmail.com>
2013-12-22 14:00:08 +0100

    send number of online members

--- src/main/scala/room.scala
+++ src/main/scala/room.scala
@@ -2,18 +2,35 @@ package collab
 
 import akka.actor._
 import akka.io.Tcp._
+import akka.util.ByteString
 import spray.can.server.websockets._
 import spray.can.server.websockets.model._
 import spray.can.server.websockets.model.OpCode._
-import spray.http._
+import scala.concurrent.duration._
+import spray.json._
+
+case object Online
 
 class Room extends Actor {
+  import context.dispatcher
 
   val sockets = scala.collection.mutable.ListBuffer.empty[ActorRef]
+
+  context.system.scheduler.schedule(
+    0.milliseconds, 1.seconds, self, Online)
 
   def receive = {
     case Sockets.Upgraded ⇒
       sockets += sender
+
+    case Online ⇒
+      sockets foreach(_ ! Frame(
+        opcode = Text,
+        maskingKey = None,
+        data = ByteString(JsObject(
+          "t" -> JsString("online"),
+          "d" -> JsNumber(sockets.length)
+        ).prettyPrint)))
 
     case f @ Frame(fin, rsv, Text | Binary, maskingKey, data) ⇒
       sockets foreach(_ ! f.copy(maskingKey = None))
